bf5a0467e5783df6b4f49d5264bceef30b3ad9c5
abteww <69enzolegg@gmail.com>
2025-01-28 17:29:45 +0100

    push

--- GreenLifeFA/resources/[ Addons ]/GreenLife/modules/[GamePlay]/Blips/client/markers.lua
+++ GreenLifeFA/resources/[ Addons ]/GreenLife/modules/[GamePlay]/Blips/client/markers.lua
@@ -1,9 +1,396 @@
 ZonesListeLTD = {
+    ["LTD-Families"] = {
+        Position = vector3(25.74, -1345.52, 29.49),
+        Public = true,
+        Job = nil,
+        Job2 = nil,
+        Blip = {
+            Name = "24/7 Supermarket",
+            Sprite = 59,
+            Display = 4,
+            Scale = 0.6,
+            Color = 2
+        },
+        Action = function()
+            OpenMenuLtd() 
+        end
+    },
+    ["LTD-Cayo"] = {
+        Position = vector3(5126.78, -5115.24, 2.21),
+        Public = true,
+        Job = nil,
+        Job2 = nil,
+        Blip = {
+            Name = "24/7 Supermarket",
+            Sprite = 59,
+            Display = 4,
+            Scale = 0.6,
+            Color = 2
+        },
+        Action = function()
+            OpenMenuLtd() 
+        end
+    },
+    ["LTD-Gs"] = {
+     Position = vector3(-48.199, -1757.80, 29.42),
+     Public = true,
+     Job = nil,
+     Job2 = nil,
+     Blip = {
+         Name = "24/7 Gasoline",
+         Sprite = 59,
+         Display = 4,
+         Scale = 0.6,
+         Color = 1
+     },
+     Action = function()
+         OpenMenuLtd() 
+     end
+     },
+     ["LTD-4"] = {
+        Position = vector3(374.17, 327.80, 103.56),
+        Public = true,
+        Job = nil,
+        Job2 = nil,
+        Blip = {
+            Name = "24/7 Supermarket",
+            Sprite = 59,
+            Display = 4,
+            Scale = 0.6,
+            Color = 2
+        },
+        Action = function()
+            OpenMenuLtd() 
+        end
+     },
+     ["LTD-5"] = {
+         Position = vector3(-1223.01, -907.29, 12.32),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Robs Liquor",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-6"] = {
+         Position = vector3(1135.59, -982.61, 46.41),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Robs Liquor",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-7"] = {
+         Position = vector3(-2967.84, 391.40, 15.04),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Robs Liquor",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-8"] = {
+         Position = vector3(-3041.06, 585.103, 7.90),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Supermarket",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-9"] = {
+         Position = vector3(-3244.08, 1001.32, 12.83),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Supermarket",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-10"] = {
+         Position = vector3(-1821.41, 793.88, 138.11),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Gasoline",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-11"] = {
+         Position = vector3(1698.2601318359,4924.3442382812,42.063694000244),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Gasoline",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-12"] = {
+         Position = vector3(1961.0549316406,3740.8920898438,32.343730926514),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Supermarket",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-13"] = {
+         Position = vector3(-1486.9412841797,-379.38009643555,40.163440704346),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Robs Liquor",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-14"] = {
+         Position = vector3(1163.5622558594,-323.66421508789,69.205047607422),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Gasoline",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-15"] = {
+         Position = vector3(2556.3720703125,382.12768554688,108.62291717529),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Supermarket",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["location"] = {
+        Position = vector3(-1034.578, -2732.765, 20.16927),
+        Public = true,
+        Job = nil,
+        Job2 = nil,
+        Blip = {
+            Name = "Location",
+            Sprite = 233,
+            Display = 4,
+            Scale = 0.6,
+            Color = 3
+        },
+        Action = function()
+            return
+        end
+    },
+     ["LTD-16"] = {
+         Position = vector3(1166.0716552734,2709.3576660156,38.15771484375),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Robs Liquor",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-17"] = {
+         Position = vector3(547.82373046875,2670.9067382812,42.156532287598),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Supermarket",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-18"] = {
+         Position = vector3(2678.3679199219,3280.6831054688,55.241115570068),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Supermarket",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-19"] = {
+         Position = vector3(1729.1472167969,6414.927734375,35.037212371826),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Supermarket",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-20"] = {
+         Position = vector3(1392.6323242188,3605.0432128906,34.980930328369),
+         Public = true,
+         Job = nil,
+         Job2 = nil,
+         Blip = {
+             Name = "24/7 Robs Liquor",
+             Sprite = 59,
+             Display = 4,
+             Scale = 0.6,
+             Color = 2
+         },
+         Action = function()
+             OpenMenuLtd() 
+         end
+     },
+     ["LTD-21"] = {
+        Position = vector3(-536.50598144531,-1221.6242675781,18.454055786133),
+        Public = true,
+        Job = nil,
+        Job2 = nil,
+        Blip = {
+            Name = "24/7 Gasoline",
+            Sprite = 59,
+            Display = 4,
+            Scale = 0.6,
+            Color = 2
+        },
+        Action = function()
+            OpenMenuLtd() 
+        end
+    },
+    ["LTD-22"] = {
+        Position = vector3(168.41580200195,-1548.5120849609,29.257730484009),
+        Public = true,
+        Job = nil,
+        Job2 = nil,
+        Blip = {
+            Name = "24/7 Gasoline",
+            Sprite = 59,
+            Display = 4,
+            Scale = 0.6,
+            Color = 2
+        },
+        Action = function()
+            OpenMenuLtd() 
+        end
+    },
+    ["fib_garage_helipad"] = {
+        Position = vector3(2505.964, -347.1071, 118.0243),
+        Public = false,
+        Job = "fib",
+        Job2 = nil,
+        Action = function()
+            OpenMenuHelipadFIB() 
+        end
+    },
     ["catalogue_plane"] = {
         Position = vector3(-963.9695, -2966.919, 13.94),
         Public = true,
         Action = function()
             TriggerEvent("openMenuCatalogue:plane")
+        end
+    },
+    ---- Vigneron --------
+    ["Garage_Vigneron"] = {
+        Position = vector3(-1923.763, 2059.459, 140.8336),
+        Public = false,
+        Job = "vigneron",
+        Job2 = nil,
+        Action = function()
+            GarageVigneron()
         end
     },
 }
@@ -50,7 +437,7 @@ CreateThread(function()
 
                 if dist < 15 then
                     Interval = 0
-                    DrawMarker(2, v.Position.x, v.Position.y, v.Position.z, 0, 0, 0, 0, nil, nil, 0.55, 0.55, 0.55, 255, 250, 0, 170, 0, 1, 0, 0, nil, nil, 0)
+                    DrawMarker(6, v.Position.x, v.Position.y, (v.Position.z - 0.97), 0, 0, 0, -90.0,0.0,0.0, 0.55, 0.55, 0.60, 30, 255, 0, 255, 0, 1, 0, 0, nil, nil, 0)
                 
                     if dist < 3 then
                         ESX.ShowHelpNotification("Appuyez sur E pour intéragir avec le menu")
@@ -69,22 +456,22 @@ end)
 
 
 
-function AddZones(zoneName, data)
-    if not ZonesListeLTD[zoneName] then
-        ZonesListeLTD[zoneName] = data
-        -- print("Creation d'une zone (ZoneName:"..zoneName..")")
-        return true
-    else 
-        -- print("Tentative de cree une zone qui exise deja (ZoneName:"..zoneName..")")
-        return false
-    end
-end
+-- function AddZones(zoneName, data)
+--     if not ZonesListeLTD[zoneName] then
+--         ZonesListeLTD[zoneName] = data
+--         -- print("Creation d'une zone (ZoneName:"..zoneName..")")
+--         return true
+--     else 
+--         -- print("Tentative de cree une zone qui exise deja (ZoneName:"..zoneName..")")
+--         return false
+--     end
+-- end
 
-local function RemoveZone(zoneName)
-    if ZonesListeLTD[zoneName] then
-        ZonesListeLTD[zoneName] = nil
-        -- print("Suppression d'une zone (ZoneName:"..zoneName..")")
-    else 
-        -- print("Tentative de supprimer une zone qui exise pas (ZoneName:"..zoneName..")")
-    end
-end
+-- local function RemoveZone(zoneName)
+--     if ZonesListeLTD[zoneName] then
+--         ZonesListeLTD[zoneName] = nil
+--         -- print("Suppression d'une zone (ZoneName:"..zoneName..")")
+--     else 
+--         -- print("Tentative de supprimer une zone qui exise pas (ZoneName:"..zoneName..")")
+--     end
+-- end
